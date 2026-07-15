13ac61cd7e3d79a7938fdd51053e5ef1e5712f4b
Rakshit <rakshitverma034@gmail.com>
2026-07-15 18:39:21 +0000

    fix: Fix the no such table error

--- app.py
+++ app.py
@@ -1275,6 +1275,44 @@ FORM_DEFINITIONS = {
 
 # List of technical names of forms defined(SQL Names)
 form_name_list = list(FORM_DEFINITIONS.keys())
+
+# Create tables for all the forms in FORM_DEFINITIONS
+for form in form_name_list:
+    # Check if table named the form exists
+    table_exists = db.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", form)
+
+    # If not exists
+    if not table_exists:
+
+        # List to store differnet fields definition
+        col_def_list = []
+        for field_col in FORM_DEFINITIONS[form]["fields"]:
+
+            field_col_name = field_col["field_name"]
+
+            # Defining form fields with dataype TEXT and is REQUIRED
+            col_def = f"{field_col_name} TEXT NOT NULL"
+            col_def_list.append(col_def)
+
+        # SQL string
+        field_cols_sql = ",".join(col_def_list)
+
+        # Dynamically create SQL tables for all forms
+        db.execute(
+            f"""CREATE TABLE IF NOT EXISTS {form}(
+            entry_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            student_id INTEGER NOT NULL,
+            {field_cols_sql},
+            full_path TEXT NOT NULL,
+            google_file_id TEXT NOT NULL DEFAULT 'pending',
+            status TEXT DEFAULT 'pending' NOT NULL,
+            submitted_at TIMESTAMP NOT NULL DEFAULT (datetime('now', '+5 hours', '+30 minutes')),
+            withdrawn_at TIMESTAMP, rejection_note TEXT,
+            FOREIGN KEY (student_id) REFERENCES student_details(student_user_id),
+            CHECK (status IN ('pending', 'accepted', 'rejected'))
+            )"""
+        )
+
 # List of title names of forms defined
 form_title = []
 form_values = []
@@ -1312,42 +1350,6 @@ db.execute("""
     contact TEXT NOT NULL DEFAULT 'to be updated',
     FOREIGN KEY (faculty_user_id) REFERENCES users(user_id))
     """)
-# Create tables for all the forms in FORM_DEFINITIONS
-for form in form_name_list:
-    # Check if table named the form exists
-    table_exists = db.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", form)
-
-    # If not exists
-    if not table_exists:
-
-        # List to store differnet fields definition
-        col_def_list = []
-        for field_col in FORM_DEFINITIONS[form]["fields"]:
-
-            field_col_name = field_col["field_name"]
-
-            # Defining form fields with dataype TEXT and is REQUIRED
-            col_def = f"{field_col_name} TEXT NOT NULL"
-            col_def_list.append(col_def)
-
-        # SQL string
-        field_cols_sql = ",".join(col_def_list)
-
-        # Dynamically create SQL tables for all forms
-        db.execute(
-            f"""CREATE TABLE IF NOT EXISTS {form}(
-            entry_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            student_id INTEGER NOT NULL,
-            {field_cols_sql},
-            full_path TEXT NOT NULL,
-            google_file_id TEXT NOT NULL DEFAULT 'pending',
-            status TEXT DEFAULT 'pending' NOT NULL,
-            submitted_at TIMESTAMP NOT NULL DEFAULT (datetime('now', '+5 hours', '+30 minutes')),
-            withdrawn_at TIMESTAMP, rejection_note TEXT,
-            FOREIGN KEY (student_id) REFERENCES student_details(student_user_id),
-            CHECK (status IN ('pending', 'accepted', 'rejected'))
-            )"""
-        )
 
 # Create a table to store and map drive folder ids
 db.execute("""
