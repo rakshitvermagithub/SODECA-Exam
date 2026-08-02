251ff43a7dd0187215364153a47d9823c0f6b9c2
Rakshit <rakshitverma034@gmail.com>
2026-08-02 10:27:54 +0000

    feat: Fetch and use sem, branch and section from individual forms

--- app.py
+++ app.py
@@ -4243,8 +4243,9 @@ def student_report():
         # Without filter
         for form in form_name_list:
             base_queries.append(
-                f"""SELECT s.student_name, s.university_roll_no, s.semester, s.branch, s.section,
-                '{form}' AS category, f.entry_id, f.google_file_id, f.submitted_at, f.withdrawn_at , f.status, f.certificate
+                f"""SELECT s.student_name, s.university_roll_no,
+                '{form}' AS category, f.entry_id, f.google_file_id, f.submitted_at, f.withdrawn_at , f.status, f.certificate,
+                f.sem, f.branch, f.section, f.academic_session, f.academic_term
                 FROM student_details s INNER JOIN {form} f ON s.student_user_id = f.student_id"""
                 )
         complete_query = " UNION ALL ".join(base_queries) 
